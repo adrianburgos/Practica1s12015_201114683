5e779ad4278f71e65cf2ca0fca3d2ead66ffdbdf
adrianburgos <voqua@hotmail.com>
2015-02-21 19:50:05 -0600

    Tablero
    genera los cuadrados para el tablero

---
+++ PlantsVsZombies/src/plantsvszombies/PlantasVsSombis.java
@@ -0,0 +1,14 @@
+package plantsvszombies;
+
+/**
+ *
+ * @author Adrian
+ */
+public class PlantasVsSombis {
+    public static void main(String[] args) 
+    {
+        fPrincipal ventana = new fPrincipal();
+        ventana.setVisible(true);
+    }
+    
+}
